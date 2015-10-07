d3c2a332a20d73ac65246e987980331341da1b56
tettoffensive <stuart.tett@gmail.com>
2015-10-07 10:30:05 -0700

    have block return keyPath

--- FBKVOController/FBKVOController.h
+++ FBKVOController/FBKVOController.h
@@ -13,10 +13,10 @@
  @abstract Block called on key-value change notification.
  @param observer The observer of the change.
  @param object The object changed.
+ @param keyPath The keyPath changed.
  @param change The change dictionary.
  */
-typedef void (^FBKVONotificationBlock)(id observer, id object, NSDictionary *change);
-
+typedef void (^FBKVONotificationBlock)(id observer, id object, NSString* keyPath, NSDictionary *change);
 
 /**
  @abstract FBKVOController makes Key-Value Observing simpler and safer.
